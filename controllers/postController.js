520a7729bdf43b2e7d497a83b2aef24e560f5028
Gabbocnc <cianci.gabriele@hotmail.it>
2024-11-04 16:00:37 +0100

    edit markup

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,11 +1,20 @@
 const post = require('../data/db.js')
 
 function index(req,res){
-  const markup = `
-  <div>${post.title}</div>
-  `;
+    const markup = `
+        ${post.map(post=> `
+            <ul>
+            <li>${post.title}</li>
+            <li>${post.slug}</li>
+            <li>${post.content}</li>
+            <li>${post.image}</li>
+            <li>${post.tags}</li>
+            </ul>
+             `
+        )}
+    `;
   /* res.send(markup) */
-  res.json(markup)
+  res.send(markup)
 
 
 }
